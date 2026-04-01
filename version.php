0ce735d4832f86e7b960a2264e99c24a0ae6942f
Mikko Haiku <haiksu@kali.alauda.local>
2026-04-01 14:49:59 +0300

    Release 1.0.0

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2026032200;
-$plugin->release   = '0.0.1';
+$plugin->release   = '1.0.0';
 $plugin->requires  = 2024100700;
 $plugin->component = 'tiny_fileimport';
