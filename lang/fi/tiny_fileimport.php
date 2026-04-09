ad9874b29eef360ddab73791f76d00c7d3662fd2
Mikko Haiku <haiksu@kali.alauda.local>
2026-04-09 12:03:48 +0300

    Fix tests

--- lang/fi/tiny_fileimport.php
+++ lang/fi/tiny_fileimport.php
@@ -23,17 +23,17 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Tiedostojen tuonti';
-$string['buttontitle'] = 'Tiedostojen tuonti';
-$string['modaltitle'] = 'Lisää tiedostoja';
-$string['dropzonehint'] = 'Vedä ja pudota tiedostoja tähän tai valitse tiedostot napsauttamalla';
 $string['allowalltypes'] = 'Salli kaikki tiedostotyypit';
 $string['allowalltypes_desc'] = 'Jos käytössä, latauksia ei rajoiteta sivuston hallinnon > Palvelin > Tiedostotyypit -osiossa lueteltuihin tiedostotyyppeihin. Jos poistettu käytöstä, kaikki siellä luetellut tiedostotyypit sallitaan oletuksena.';
-$string['overridedefaultfileattachmentfeature'] = 'Ohita oletusarvoinen tiedostojen liittämistoiminto';
-$string['overridedefaultfileattachmentfeature_desc'] = 'Jos käytössä, tämä lisäosa käsittelee editorin vedä ja pudota -lataukset Tiny:n oletusarvoisen tiedostojen liittämiskäsittelyn sijaan. Jos pois käytöstä, editori säilyttää oman oletuslatauksensa tuetuille tiedostoille, kuten kuville, ja tätä lisäosaa käytetään vain tiedostoille, joita editorin oletuslataus ei käsittele.';
 $string['allowedextensionsoverride'] = 'Sallittujen tiedostopäätteiden ohitus';
 $string['allowedextensionsoverride_desc'] = 'Valinnainen. Pilkulla, välilyönnillä tai rivinvaihdolla erotettu luettelo sallituista tiedostopäätteistä (esimerkiksi: pdf, docx, xlsx, zip). Jos tyhjä, lisäosa käyttää täydellistä luetteloa sivuston hallinnosta > Palvelin > Tiedostotyypit. Ohitetaan, kun "Salli kaikki tiedostotyypit" on käytössä.';
+$string['buttontitle'] = 'Tiedostojen tuonti';
+$string['dropzonehint'] = 'Vedä ja pudota tiedostoja tähän tai valitse tiedostot napsauttamalla';
+$string['fileimport:use'] = 'Käytä Tiny-tiedostojen tuontia';
 $string['filetypenotsupported'] = 'Tiedostotyyppi ei ole tuettu';
 $string['filetypenotsupported_desc'] = 'Tiedostoa "{$a}" ei voitu ladata, koska sen tiedostotyyppi ei ole tuettu nykyisten asetusten mukaan.';
-$string['fileimport:use'] = 'Käytä Tiny-tiedostojen tuontia';
+$string['modaltitle'] = 'Lisää tiedostoja';
+$string['overridedefaultfileattachmentfeature'] = 'Ohita oletusarvoinen tiedostojen liittämistoiminto';
+$string['overridedefaultfileattachmentfeature_desc'] = 'Jos käytössä, tämä lisäosa käsittelee editorin vedä ja pudota -lataukset Tiny:n oletusarvoisen tiedostojen liittämiskäsittelyn sijaan. Jos pois käytöstä, editori säilyttää oman oletuslatauksensa tuetuille tiedostoille, kuten kuville, ja tätä lisäosaa käytetään vain tiedostoille, joita editorin oletuslataus ei käsittele.';
+$string['pluginname'] = 'Tiedostojen tuonti';
 $string['privacy:metadata'] = 'Tiny-tiedostojen tuonti -lisäosa ei tallenna henkilötietoja.';
